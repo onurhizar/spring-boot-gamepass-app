3bd4ee1ed5b4acb154047b4bac96895fec63e8e6
onurhizar <onurhizar@tutanota.com>
2023-03-17 21:34:10 +0300

    feat: init database connection

---
+++ src/main/java/com/onurhizar/gamepass/GamePassApplication.java
@@ -0,0 +1,13 @@
+package com.onurhizar.gamepass;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GamePassApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GamePassApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/onurhizar/gamepass/GamePassApplicationTests.java
@@ -0,0 +1,13 @@
+package com.onurhizar.gamepass;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GamePassApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
