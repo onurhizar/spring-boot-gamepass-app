24d454a31a2d4d7415f09f62d45f8014a8668138
onurhizar <onurhizar@tutanota.com>
2023-03-25 02:44:31 +0300

    feat: validation
    some examples are done on 
    - LoginRequest
    - RegisterRequest

--- src/main/java/com/onurhizar/gamepass/model/request/auth/LoginRequest.java
+++ src/main/java/com/onurhizar/gamepass/model/request/auth/LoginRequest.java
@@ -1,5 +1,7 @@
 package com.onurhizar.gamepass.model.request.auth;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.*;
 
 @Data
@@ -7,7 +9,11 @@ import lombok.*;
 @AllArgsConstructor
 public class LoginRequest {
 
+    @NotBlank
     private String username;
+
+    @NotBlank
+    @Size(min = 6, max = 32)
     private String password;
 
 }
