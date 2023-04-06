61e01c94cd544c0ec6d31242be9c73460be84575
onurhizar <onurhizar@tutanota.com>
2023-04-06 22:05:26 +0300

    Change Test Properties File Name
    Also added some tests for user controller.

--- src/test/java/com/onurhizar/gamepass/GamePassApplicationTests.java
+++ src/test/java/com/onurhizar/gamepass/GamePassApplicationTests.java
@@ -5,7 +5,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
-@ActiveProfiles("test")
+@ActiveProfiles("integration-test")
 class GamePassApplicationTests {
 
 	@Test
