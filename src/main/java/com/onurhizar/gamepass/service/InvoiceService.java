f6a3f3da49f4d2c8dc494f0e9ded391c2ec57655
onurhizar <onurhizar@tutanota.com>
2023-05-19 13:38:53 +0300

    Change ScheduleService Cron Expressions for Production
    Incorporated the cron expression to repeat every 2 minutes for testing purposes. In production, invoices will now be generated at the end of each month and checked for any outstanding payments after a 15-day grace period.

--- src/main/java/com/onurhizar/gamepass/service/InvoiceService.java
+++ src/main/java/com/onurhizar/gamepass/service/InvoiceService.java
@@ -13,6 +13,7 @@ import javax.transaction.Transactional;
 
 import com.onurhizar.gamepass.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 import java.time.ZonedDateTime;
@@ -20,6 +21,7 @@ import java.util.List;
 
 @Service
 @RequiredArgsConstructor
+@Slf4j
 public class InvoiceService {
     private final InvoiceRepository repository;
     private final PaymentService paymentService;
@@ -61,26 +63,22 @@ public class InvoiceService {
 
         paymentService.addPayment(payment);
 
+        if (remainingFee!=0) return invoice;
+
         /* if remaining fee is 0, then invoice is paid.
         Check if user is GUEST and upgrade to MEMBER if all invoices are paid */
-        if (remainingFee==0){
-            ContractRecord contractRecord = invoice.getContractRecord();
-            User user = contractRecord.getUser();
-            if (user.getRole().equals(UserRole.GUEST)){
-                // check if all past-due invoices have been paid, if true, make user MEMBER again
-                //List<Invoice> unpaidInvoices = repository.findByContractRecordAndFeeIsNot(contractRecord.getId(), 0);
-                List<Invoice> unpaidInvoices = findAllNonPaidPastDueInvoicesForSpecificUser(user.getId());
-                System.out.println("unpaid invoices:"+unpaidInvoices.size());
-                for (Invoice unpaidInvoice : unpaidInvoices) {
-                    System.out.println(unpaidInvoice);
-                }
-                // TODO note that, if user role is ADMIN, new role will be MEMBER. Ignore this possibility
-                if (unpaidInvoices.isEmpty()) {
-                    upgradeUserRoleToMember(user.getId());
-                    System.out.println("User is UPGRADED to MEMBER");
-                }
+        ContractRecord contractRecord = invoice.getContractRecord();
+        User user = contractRecord.getUser();
 
-            }
+        if (!user.getRole().equals(UserRole.GUEST)) return invoice;
+
+        List<Invoice> unpaidInvoices = findAllNonPaidPastDueInvoicesForSpecificUser(user.getId());
+        log.info("unpaid invoices:"+unpaidInvoices.size());
+
+        // TODO note that, if user role is ADMIN, new role will be MEMBER. Ignore this possibility
+        if (unpaidInvoices.isEmpty()) {
+            upgradeUserRoleToMember(user.getId());
+            log.info("User is UPGRADED to MEMBER, usedId: " + user.getId());
         }
 
         return invoice;
@@ -90,18 +88,10 @@ public class InvoiceService {
         return repository.findById(id).orElseThrow(EntityNotFoundException::new);
     }
 
-    /**
-     * to list invoices that are older than 15 days from now so that we can downgrade the user to guest role
-     */
+    /** to list invoices that are older than 15 days from now so that we can downgrade the user to guest role */
     public List<Invoice> findInvoicesBy15DaysOld(){
         ZonedDateTime the15DaysOldTime = ZonedDateTime.now().minusDays(15);
         return repository.findByCreatedAtBefore(the15DaysOldTime);
-    }
-
-    // TODO remove this method
-    public List<Invoice> findNonPaidInvoicesBy5MinsOld(){
-        ZonedDateTime the5MinsOldTime = ZonedDateTime.now().minusMinutes(5).plusSeconds(2);
-        return repository.findByCreatedAtBeforeAndFeeIsNot(the5MinsOldTime, 0);
     }
 
     public List<Invoice> findAllNonPaidPastDueInvoicesForSpecificUser(String userId){
@@ -109,7 +99,7 @@ public class InvoiceService {
         return repository.findByContractRecordUserIdAndFeeNotAndCreatedAtBefore(userId, 0, the5MinsOldTime);
     }
 
-    // TODO remove this method, resolve circular dependency
+    // TODO remove this method later, resolve circular dependency
     private void upgradeUserRoleToMember(String userId){
         User user = userRepository.findById(userId).orElseThrow(EntityNotFoundException::new);
         user.setRole(UserRole.MEMBER);
--- src/main/java/com/onurhizar/gamepass/service/ScheduleService.java
+++ src/main/java/com/onurhizar/gamepass/service/ScheduleService.java
@@ -2,7 +2,6 @@ package com.onurhizar.gamepass.service;
 
 import com.onurhizar.gamepass.model.entity.ContractRecord;
 import com.onurhizar.gamepass.model.entity.Invoice;
-import com.onurhizar.gamepass.repository.InvoiceRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.scheduling.annotation.EnableScheduling;
@@ -21,42 +20,33 @@ public class ScheduleService {
     private final ContractRecordService contractRecordService;
     private final InvoiceService invoiceService;
     private final UserService userService;
-    private final InvoiceRepository invoiceRepository;
 
-    @Scheduled(cron = "0 */2 * * * *")
-    public void createInvoicesInEveryEvenMinute(){
-        log.info("1 minute has passed");
-
-        // for each contract record, create invoice
+    // FOR TESTING PURPOSES you can use "0 */2 * * * *" to run every 2 minutes (even minutes)
+    @Scheduled(cron = "0 0 0 L * *")
+    public void createInvoicesInLastDayOfMonth(){
         List<ContractRecord> activeContractRecords = contractRecordService.findActiveContractRecords();
+        log.info("Create invoices for active contract records");
         log.info("Active contract records: "+activeContractRecords.size());
 
         for (ContractRecord contractRecord : activeContractRecords) {
-            log.info("Contract record: "+contractRecord);
             contractRecordService.createInvoice(contractRecord);
         }
 
     }
 
-    @Scheduled(cron = "0 1/2 * * * *")
-    public void createInvoicesInEveryOddMinute(){
-        // if not paid invoice, make the user guest status TODO
-        log.info("Odd minute");
-        findPassedInvoicesAndExtractTheirUserIds();
-
-        List<Invoice> invoices = invoiceService.findAllNonPaidPastDueInvoicesForSpecificUser("aa59d163-5e7e-4290-b6ac-b901b0b4543a"); // TODO remove this line
-        log.info("Specific user invoices:");
-        for (Invoice invoice : invoices) {
-            log.info("Invoice: "+invoice);
-        }
+    // FOR TESTING PURPOSES you can use "0 1/2 * * * *" to run every 2 minutes (odd minutes)
+    @Scheduled(cron = "0 0 0 15 * *")
+    public void checkUnpaidPastInvoicesIn15thDayOfMonth(){
+        log.info("Check for non paid invoices and downgrade corresponding users role to guest");
+        HashSet<String> userIds = findPastInvoicesAndExtractTheirUserIds();
+        downgradeUsersToGuestStatus(userIds);
     }
 
-    private void findPassedInvoicesAndExtractTheirUserIds(){
+    private HashSet<String> findPastInvoicesAndExtractTheirUserIds(){
         HashSet<String> userIds = new HashSet<>();
 
-        //List<Invoice> oldInvoices = invoiceService.findInvoicesBy15DaysOld(); // 15 days passed invoices // todo use 15 old
-        List<Invoice> oldInvoices = invoiceService.findNonPaidInvoicesBy5MinsOld(); // 15 days passed invoices // todo use 15 old
-        log.info("Old Non Paid invoices: "+oldInvoices.size()); // TODO make it only non zero fee invoices
+        List<Invoice> oldInvoices = invoiceService.findInvoicesBy15DaysOld(); // 15 days passed invoices // todo use 15 old
+        log.info("Old Non Paid invoices: "+oldInvoices.size());
         for (Invoice invoice : oldInvoices) {
             log.info("Invoice: "+invoice);
             log.info("UserID: "+invoice.getContractRecord().getUser().getId());
@@ -64,16 +54,11 @@ public class ScheduleService {
             userIds.add(invoice.getContractRecord().getUser().getId());
         }
 
-        downgradeUsersToGuestStatus(userIds);
+        return userIds;
     }
 
     private void downgradeUsersToGuestStatus(HashSet<String> userIds){
         for (String userId : userIds) userService.downgradeUserRoleToGuest(userId);
     }
 
-
-    // seconds minutes hours days months day-of-week
-    // to repeat in every 2 minutes : 0 */2 * * * *
-    // every even minute 0 */2 * * * *
-    // every odd minute 0 1/2 * * * *
 }
