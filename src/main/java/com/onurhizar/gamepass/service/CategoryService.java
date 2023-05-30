15b5f3e30ac30234f26cb35aeb6742c5365438e8
onurhizar <onurhizar@tutanota.com>
2023-05-30 11:44:11 +0300

    Change Stack to Deque for Performance

--- src/main/java/com/onurhizar/gamepass/service/CategoryService.java
+++ src/main/java/com/onurhizar/gamepass/service/CategoryService.java
@@ -1,6 +1,7 @@
 package com.onurhizar.gamepass.service;
 
 import com.onurhizar.gamepass.exception.EntityNotFoundException;
+import com.onurhizar.gamepass.exception.UnacceptableRequestException;
 import com.onurhizar.gamepass.model.entity.Category;
 import com.onurhizar.gamepass.model.entity.Game;
 import com.onurhizar.gamepass.model.entity.User;
@@ -11,11 +12,10 @@ import com.onurhizar.gamepass.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
 
+import java.util.Deque;
 import java.util.LinkedList;
 import java.util.List;
-import java.util.Stack;
 
 @Service
 @RequiredArgsConstructor
@@ -66,21 +66,20 @@ public class CategoryService {
                 .orElseThrow(EntityNotFoundException::new);
 
         if (category.getId().equals(parentCategory.getId()))
-            throw new RuntimeException("a category cannot be its parent, same name is disallowed"); // TODO name
+            throw new UnacceptableRequestException("a category cannot be its parent, same name is disallowed");
 
         category.setName(request.getName());
         category.setParent(parentCategory);
         return CategoryResponse.fromEntity(repository.save(category));
     }
 
-
-    // TODO check if children categories exists, handle them
+    
     /** detach users first, then delete category */
     private void deleteCategory(String id) {
         Category category = repository.findById(id)
                 .orElseThrow(EntityNotFoundException::new);
         if (category.isSuperCategory())
-            throw new RuntimeException("Super category cannot be deleted");
+            throw new UnacceptableRequestException("Super category cannot be deleted");
 
         List<User> followingUsers = userRepository.findByFollowedCategories(category);
         for (User user : followingUsers) {
@@ -92,30 +91,30 @@ public class CategoryService {
     }
 
     /** Returns a stack, the most leaf is at the top, root stays at the bottom to implement remove orphan logic */
-    public Stack<Category> findAllChildrenCategories(String categoryId){
+    public Deque<Category> findAllChildrenCategories(String categoryId){
         Category rootCategory = repository.findById(categoryId)
                 .orElseThrow(EntityNotFoundException::new);
 
-        Stack<Category> stack = new Stack<>();
-        traverseAllChildrenOfCategory(rootCategory, stack);
+        Deque<Category> deque = new LinkedList<>();
+        traverseAllChildrenOfCategory(rootCategory, deque);
 
         // we need to reverse the stack to iterate from leaf to root
-        Stack<Category> reversedStack = new Stack<>();
-        while (!stack.isEmpty()) reversedStack.push(stack.pop());
+        Deque<Category> reversedStack = new LinkedList<>();
+        while (!deque.isEmpty()) reversedStack.push(deque.pop());
         return reversedStack;
     }
 
 
     /** Returns reversed stack, starts from leaf and goes to root category */
-    private Stack<Category> traverseAllChildrenOfCategory(Category rootCategory, Stack<Category> stack) {
+    private Deque<Category> traverseAllChildrenOfCategory(Category rootCategory, Deque<Category> deque) {
         List<Category> childrenCategories = repository.findCategoriesByParentId(rootCategory.getId());
 
         if (!childrenCategories.isEmpty()) // first, iterate over children
             for (Category category : childrenCategories)
-                traverseAllChildrenOfCategory(category, stack);
+                traverseAllChildrenOfCategory(category, deque);
 
-        stack.push(rootCategory); // then push self to stack
-        return stack; // root node stays at the top of the stack, need to reverse it before using
+        deque.push(rootCategory); // then push self to stack
+        return deque; // root node stays at the top of the stack, need to reverse it before using
     }
     
     public void deleteCategoryByAssignChildrenToGrandParent(String categoryId){
@@ -129,20 +128,12 @@ public class CategoryService {
         deleteCategory(categoryId);
     }
 
-    /* orphan removal logic, remove leaf nodes first then self
-    public void deleteCategoryWithItsChildren(String categoryId){
-        Stack<Category> stack = findAllChildrenCategories(categoryId);
-        while (!stack.isEmpty()){
-            deleteCategory(stack.pop().getId());
-        }
-    }
-    */
 
     /** Appends all games of children categories' games, if same game exists in the list, does not append */
     private List<Game> appendAllGamesFromChildrenCategories(String categoryId, List<Game> games){
-        Stack<Category> stack = findAllChildrenCategories(categoryId);
+        Deque<Category> deque = findAllChildrenCategories(categoryId);
 
-        for (Category category : stack) {
+        for (Category category : deque) {
             List<Game> gamesOfChildrenCategory = category.getGames();
             for (Game game : gamesOfChildrenCategory)
                 if (!games.contains(game)) games.add(game);
